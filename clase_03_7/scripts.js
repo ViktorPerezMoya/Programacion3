39707552976723afe3bf0afdfd9be2cd04f82cef
Victor Ariel Perez <victor.ariel.perez@gmail.com>
2018-07-03 22:18:06 -0300

    Solucion alejercicio

--- clase_03_7/scripts.js
+++ clase_03_7/scripts.js
@@ -3,8 +3,33 @@ $(document).ready(function(){
         evt.preventDefault();
     });
     
+    $("#parametro").focus(function(){
+        $(this).attr("value","");
+    });
+    
+    $("#parametro").blur(function (){
+        $(this).attr("value","Buscar palabra...");
+    });
+    
+    $("#buscar").mousedown(function(){
+        //$(this).
+    });
+    
+    
     $("#buscar").click(function(){
-        console.log("ejecuta logica");
+        var array = $("p").toArray();
+        var palabra = $("#parametro").val();
+        var html = "";
+        
+        for(var i = 0; i < array.length; i++){
+            var stemp = array[i].innerHTML;
+            stemp = stemp.replace('<span class="encontrado">',"");
+            stemp = stemp.replace("</span>","");
+            console.log(stemp);
+            html += "<p>"+stemp.replace(palabra,"<span class='encontrado'>"+palabra+"</span>")+"</p>";
+        }
+        
+        $("#parrafos").html(html);
     });
     
 });
