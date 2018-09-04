debae841760a13c18edccd5803a6266c731daead
ViktorPerez <ViktorPerez@DESKTOP-QQ9KLKO>
2018-09-04 20:58:44 -0300

    listado de usuario con carga de datos en javascript

--- clase28_8/listar_usuarios.php
+++ clase28_8/listar_usuarios.php
@@ -12,6 +12,7 @@ if(empty($_SESSION['id_user'])){
         <link rel="stylesheet" href="estilos.css"/>
         <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="funciones.js"/></script>
+    <script type="text/javascript" src="usuarios.js"/></script>
     </head>
     <body>
         <div class="menu">
@@ -36,38 +37,9 @@ if(empty($_SESSION['id_user'])){
         </div>
         <div class="contenedor">
             <h1>Lista de Usuarios</h1>
-            <div class="dvtabla">
-<?php
-        $conn = mysqli_connect('localhost', 'root', '', 'bdnet', '3306');
-        if (!$conn) {
-            die('Could not connect to MySQL: ' . mysqli_connect_error());
-        }
-        mysqli_query($conn, 'SET NAMES \'utf8\'');
-        //inicio de consulta
-        echo '<table>';
-        echo '<thead>';
-        echo '<tr>';
-        echo '<th>id</th>';
-        echo '<th>username</th>';
-        echo '<th>nombre</th>';
-        echo '<th>acciones</th>';
-        echo '</tr>';
-        echo '</thead>';
-$result = mysqli_query($conn, 'SELECT id, username, nombre FROM usuario');
-while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
-            echo '<tr>';
-            echo '<td>' . $row['id'] . '</td>';
-            echo '<td>' . $row['username'] . '</td>';
-            echo '<td>' . $row['nombre'] . '</td>';
-            echo '<td> <a href="#">Editar</a> <a href="#">Borrar</a></td>';
-            echo '</tr>';
-}
-mysqli_free_result($result);
-echo '</table>';
-        //fin de consulta
-        mysqli_close($conn);
-?>
-        </div>
+            <div class="dvtabla" id="dvtabla">
+                
+            </div>
         </div>
     </body>
 </html>
